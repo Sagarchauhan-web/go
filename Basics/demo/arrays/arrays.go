0c5a01f8b1b7dfc8c00f4a43c39952113ab81f6d
Sagar <Sagarchawan4646@gmail.com>
2024-02-18 17:52:07 +0530

    Demo: arrays

--- Basics/demo/arrays/arrays.go
+++ Basics/demo/arrays/arrays.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ { 
+		room := rooms[i]; // always make an copy
+		if room.cleaned {
+			fmt.Println(room.name, "cleaned")
+		} else {
+			fmt.Println(room.name, "dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "office"},
+		{name: "warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing cleaning...")
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 }
