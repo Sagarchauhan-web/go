3a0a6cdbb8a84e217a39915107a85d5b70807922
Sagar <Sagarchawan4646@gmail.com>
2024-02-26 15:05:16 +0530

    Demo: Errors

--- Basics/demo/errors/errors.go
+++ Basics/demo/errors/errors.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) - 1 {
+		return 0, errors.New(fmt.Sprintf("index %d out of bounds", index))
+		} else {
+			return s.values[index], nil
+		}
+
+}
+
+func main() {
+		stuff := Stuff{
+			values: []int{1, 2, 3},
+		}
+		value, err := stuff.Get(6)
+
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(value)
+		}
 }
