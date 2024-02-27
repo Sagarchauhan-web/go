75adf40bc62ae46eb4c74a88365a388f5fa4b0fb
Sagar <Sagarchawan4646@gmail.com>
2024-02-28 00:44:50 +0530

    Exercise: Testing

--- Basics/exercise/testing/rcv-func_test.go
+++ Basics/exercise/testing/rcv-func_test.go
@@ -10,3 +10,54 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+
+import "testing"
+
+func newPlayer() Player {
+	return Player{name: "Sagar", health: 10, energy: 10}
+}
+
+func TestHealth(t *testing.T) {
+	p := newPlayer()
+	p.addHealth(MAX_HEALTH)
+
+	if p.health != MAX_HEALTH {
+		t.Fatalf("Expected health to be %v, got %v", MAX_HEALTH, p.health)
+	}
+
+	if p.health > MAX_HEALTH {
+		t.Fatalf("health can not be greater than %v", MAX_HEALTH)
+	}
+
+	p.applyDamage(MAX_HEALTH + 1)
+	if p.health != 0 {
+		t.Fatalf("Expected health to be 0, got %v", p.health)
+	}
+
+	if p.health < 0 {
+		t.Fatalf("health can not be less than 0")
+	}
+}
+func TestEnergy(t *testing.T) {
+	p := newPlayer()
+	p.addEnergy(MAX_ENERGY)
+
+	if p.energy != MAX_ENERGY {
+		t.Fatalf("Expected energy to be %v, got %v", MAX_ENERGY, p.energy)
+	}
+
+	if p.energy > MAX_ENERGY {
+		t.Fatalf("energy can not be greater than %v", MAX_ENERGY)
+	}
+
+	p.consumeEnergy(MAX_ENERGY + 1)
+	if p.energy != 0 {
+		t.Fatalf("Expected energy to be 0, got %v", p.energy)
+	}
+
+	if p.energy < 0 {
+		t.Fatalf("energy can not be less than 0")
+	}
+}
