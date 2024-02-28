7bf9ce4d6c2ab49fd97aac835ef33bc667945514
Sagar <Sagarchawan4646@gmail.com>
2024-02-29 04:59:26 +0530

    Demo: Dunc Literal

--- Basics/demo/func-literal/func-literal.go
+++ Basics/demo/func-literal/func-literal.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
 
+func compute(lhs, rhs int, op func(lhs int, rhs int) int) int {
+	fmt.Printf("Running a computation with %v and %v\n", lhs, rhs)
+	return op(lhs, rhs)
+}
+
+func main() {
+	fmt.Println("2 + 3 =", compute(2, 3, add))
+
+	fmt.Println("5 - 3 =", compute(5, 3, func(lhs, rhs int) int {
+		return lhs - rhs
+	}))
+
+	mul := func(lhs, rhs int) int {
+		fmt.Printf("Running a multiplication with %v and %v\n", lhs, rhs)
+		return lhs * rhs
+	}
+
+	fmt.Println(compute(3, 3, mul))
 }
