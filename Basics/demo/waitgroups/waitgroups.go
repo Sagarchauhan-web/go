b11d29c19c4f17326080846c1cabdfe6bca1df49
Sagar <Sagarchawan4646@gmail.com>
2024-03-05 02:27:46 +0530

    Demo: waitgroups

--- Basics/demo/waitgroups/waitgroups.go
+++ Basics/demo/waitgroups/waitgroups.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"sync"
 )
 
 func main() {
+    var wg sync.WaitGroup
+    sum := 0
+
+    for i := 0; i < 10; i++ {
+        wg.Add(1);
+        value := i
+
+        go func() { 
+            defer wg.Done()
+            sum += value;
+        }()
+    }
+
+    wg.Wait()
+    fmt.Println("Sum:", sum)
 }
